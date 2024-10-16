fc305a9cce579da349faf0018b0e0f1c84882805
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2024-10-16 08:27:58 -0500

    increase image size

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use tracer::vector::Vector;
 fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 800;
     let image_height = (image_width as f64 / aspect_ratio) as usize;
     let samples_per_pixel = 100;
     let max_depth = 50;
